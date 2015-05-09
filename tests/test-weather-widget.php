86aaa935454837a114bbf95ea88a3386f813089d
Mark Chouinard <mark@chouinard.me>
2015-05-09 15:38:29 -0700

    Add/edit unit tests

--- tests/test-weather-widget.php
+++ tests/test-weather-widget.php
@@ -9,6 +9,8 @@ class WeatherWidgetTest extends WP_UnitTestCase {
 
 		\WP_Mock::setUp();
 
+		add_action( 'widgets_init', 'machouinard_adds_register_widget' );
+
 		\WP_Mock\Handler::register_handler(
 			'sanitize_text_field',
 			function( $str ){
@@ -30,21 +32,68 @@ class WeatherWidgetTest extends WP_UnitTestCase {
 		$this->assertTrue( true );
 	}
 
+
 	/**
 	 * @covers  Machouinard_Adds_Weather_Widget::clean_icao()
 	 */
 	function testClean_icao() {
-		$icao = Machouinard_Adds_Weather_Widget::clean_icao( 'smf' );
-		$expected = 'KSMF';
-		$this->assertEquals( $icao, $expected );
+		$actual = Machouinard_Adds_Weather_Widget::clean_icao( 'gll' );
+		$expected = 'EGLL';
+		$this->assertEquals( $actual, $expected );
 	}
 
 	/**
 	 * @covers  Machouinard_Adds_Weather_Widget::clean_icao()
 	 */
 	function testClean_icao_two() {
-		$icao = Machouinard_Adds_Weather_Widget::clean_icao( 'dtw' );
+		$actual = Machouinard_Adds_Weather_Widget::clean_icao( 'yvr' );
+		$expected = 'CYVR';
+		$this->assertEquals( $actual, $expected );
+	}
+
+	/**
+	 * @covers  Machouinard_Adds_Weather_Widget::clean_icao()
+	 */
+	function testClean_icao_three() {
+		$actual = Machouinard_Adds_Weather_Widget::clean_icao( 'qqq' );
+		$expected = 'KQQQ';
+		$this->assertNotEquals( $actual, $expected );
+	}
+
+	/**
+	 * @covers  Machouinard_Adds_Weather_Widget::clean_icao()
+	 */
+	function testClean_icao_four() {
+		$actual = Machouinard_Adds_Weather_Widget::clean_icao( 'qqq' );
+		$expected = 'QQQ';
+		$this->assertEquals( $actual, $expected );
+	}
+
+	/**
+	 * @covers  Machouinard_Adds_Weather_Widget::clean_icao()
+	 */
+	function testClean_icao_five() {
+		$actual = Machouinard_Adds_Weather_Widget::clean_icao( 'KDTW' );
 		$expected = 'KDTW';
+		$this->assertEquals( $actual, $expected );
+	}
+
+	/**
+	 * @covers  Machouinard_Adds_Weather_Widget::clean_icao()
+	 */
+	function testClean_icao_six() {
+		$actual = Machouinard_Adds_Weather_Widget::clean_icao( 'ssy' );
+		$expected = 'YSSY';
+		$this->assertEquals( $actual, $expected );
+	}
+
+	/**
+	 * @covers  Machouinard_Adds_Weather_Widget::clean_icao()
+	 */
+	function testClean_icao_lowercase() {
+		$actual = Machouinard_Adds_Weather_Widget::clean_icao( 'dtw' );
+		$expected = 'kdtw';
+		$this->assertNotEquals( $actual, $expected );
 	}
 
 	/**
@@ -54,7 +103,8 @@ class WeatherWidgetTest extends WP_UnitTestCase {
 		$metar = Machouinard_Adds_Weather_Widget::get_metar( 'KSMF', 4 );
 		$this->assertArrayHasKey( 'metar', $metar );
 		$count = count( $metar['metar'] );
-//		$this->assertGreaterThan(1, $count, '1 should be greater than ' . $count );;
+		$expected = 1;
+		$this->assertGreaterThanOrEqual( $expected, $count );;
 	}
 
 	/**
@@ -64,6 +114,7 @@ class WeatherWidgetTest extends WP_UnitTestCase {
 		$pireps = Machouinard_Adds_Weather_Widget::get_pireps( 'KSEA', 120, 5 );
 		$count = count ( $pireps );
 		$this->assertGreaterThanOrEqual( 0, $count );
+		$this->assertArrayHasKey( 0, $pireps );
 	}
 
 
@@ -80,6 +131,27 @@ class WeatherWidgetTest extends WP_UnitTestCase {
 		$this->assertNotEquals($expected, $bad_count );
 		$expected_city = 'ZANESVILLE';
 		$this->assertEquals( $apt['city'], $expected_city );
+	}
+
+	function testGet_apt_info_two() {
+		$apt = 'EGLL';
+		$info = Machouinard_Adds_Weather_Widget::get_apt_info( $apt );
+		$count = count( $info );
+		$this->assertEquals( 4, $count );
+		$this->assertArrayHasKey( 'city', $info );
+		$this->assertEquals( $info['city'], 'LONDON/HEATHROW' );
+	}
+
+	function testGet_apt_info_three() {
+		$apt = 'QQQQ';
+		$info = Machouinard_Adds_Weather_Widget::get_apt_info( $apt );
+		$this->assertFalse( $info );
+	}
+
+	function testLoad_xml() {
+		$url = 'http://chouinard.me';
+		$result = Machouinard_Adds_Weather_Widget::load_xml( $url );
+		$this->assertFalse( $result );
 	}
 
 }
