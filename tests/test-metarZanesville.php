50f98269ecdc421dc40d9a77ace9c0a640a82aab
Mark Chouinard <mark@chouinard.me>
2016-02-07 01:17:29 -0800

    unit test

---
+++ tests/test-metarZanesville.php
@@ -0,0 +1,38 @@
+<?php
+
+class TestMetarZanesville extends WP_UnitTestCase {
+
+	private $zzv;
+
+
+	public function setUp() {
+
+		$this->zzv = new AwfnMetar( 'kzzv', 4, false );
+		$this->zzv->go( true );
+	}
+
+	public function tearDown() {
+
+	}
+
+	function testDefaultMetarHours() {
+		$this->assertEquals( 4, $this->zzv->get_hours() );
+	}
+
+	function testStationCase() {
+		$this->assertEquals( 'kzzv', $this->zzv->station );
+	}
+
+	function testStationShow() {
+		$this->assertEquals( false, $this->zzv->get_show() );
+	}
+
+	function testMetarXmlData() {
+		$xmlData = $this->zzv->xmlData;
+		$this->assertEquals( 'KZZV', $xmlData['station_id'] );
+		$this->assertEquals( 39.95, (float) $xmlData['latitude'] );
+		$this->assertEquals( -81.9, (float) $xmlData['longitude'] );
+		$this->assertEquals( 'METAR', $xmlData['metar_type'] );
+	}
+
+}
