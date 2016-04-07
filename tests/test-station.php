47938189185cc28085229c773d0802533b0b7bba
Mark Chouinard <mark@chouinard.me>
2016-04-07 16:44:34 -0700

    expand static test

--- tests/test-station.php
+++ tests/test-station.php
@@ -13,7 +13,21 @@ class StationTest extends WP_UnitTestCase {
 	}
 
 	function testStaticInfo() {
-		$this->assertArrayHasKey( 'country', AwfnStation::static_apt_info('ksmf') );
+		$icao = 'KOKC';
+		// Check our stored option for matching ICAO data
+		$stations = get_option( STORED_STATIONS_KEY, array() );
+		// Our test station should not be found in the option
+		$this->assertFalse( isset( $stations[$icao] ) );
+
+		$info = AwfnStation::static_apt_info($icao);
+		$this->assertArrayHasKey( 'station_id', $info );
+		$this->assertArrayHasKey( 'latitude', $info );
+		$this->assertArrayHasKey( 'longitude', $info );
+		$this->assertArrayHasKey( 'country', $info );
+
+		$stations = get_option( STORED_STATIONS_KEY, array() );
+		// Our test station should have been added to the option for caching
+		$this->assertTrue( isset( $stations[$icao] ) );
 	}
 
 	function testStationExists() {
